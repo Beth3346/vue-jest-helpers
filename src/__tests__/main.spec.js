2e1cddc0a14eea42be4d60816ac576acd130b9c1
Elizabeth Rogers <beth3346@gmail.com>
2018-07-11 18:36:36 -0500

    remove index.js; update tests

--- src/__tests__/main.spec.js
+++ src/__tests__/main.spec.js
@@ -1,11 +1,7 @@
-import helpers from '../../index';
+import helpers from '../index';
 
 describe('helpers', () => {
   it('should have some methods', () => {
-    console.log(helpers);
-
-    console.log(Object.keys(helpers.init()).length);
-
     expect(Object.keys(helpers).length).toBeGreaterThan(0);
   });
 });
